f494b8eb5132ee37ba80acbb2d8b576e3dd28ff5
modernSE Trainee 4 <keydon+mse-trainee@gmail.com>
2018-02-20 11:33:45 +0100

    Ergebnis der Gruppe 4

--- src/main/java/de/cas/mse/exercise/SumDigPower.java
+++ src/main/java/de/cas/mse/exercise/SumDigPower.java
@@ -8,7 +8,9 @@ public class SumDigPower {
 	public List<Long> findNumbersEqualToSumOfTheirDigitPowers(long rangeStart, long rangeEnd) {
 		List<Long> numbers = new ArrayList<Long>();
 
+		//Umbenennung i z.b. in number
 		for (long i = rangeStart; i < rangeEnd; i++) {
+			//evtl. auslagern in Funktion
 			List<Long> digits = splitIntoDigits(i);
 			long sum = calculateSumOfPowers(digits);
 
@@ -25,6 +27,7 @@ public class SumDigPower {
 		String numberToSplit = Long.toString(number);
 
 		for (int j = 0; j < numberToSplit.length(); j++) {
+			// Long.valueOf(numberToSplit.substring(j, j + 1)) kann man extrahieren
 			digits.add(Long.valueOf(numberToSplit.substring(j, j + 1)));
 		}
 
@@ -33,7 +36,9 @@ public class SumDigPower {
 
 	private long calculateSumOfPowers(List<Long> digits) {
 		long sum = 0;
+		//j umbenennen
 		for (int j = 1; j <= digits.size(); j++) {
+			//evtl neue variable für Potenz -> verständlicher
 			sum += Math.pow(digits.get(j - 1), j);
 		}
 		return sum;
