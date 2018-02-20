7cd73a21d79034530751a11f3a9f5b0b97a95c66
modernSE Trainee 3 <keydon+mse-trainee@gmail.com>
2018-02-20 11:22:31 +0100

    Ergebnis der Gruppe 3

--- src/main/java/de/cas/mse/exercise/SumDigPower.java
+++ src/main/java/de/cas/mse/exercise/SumDigPower.java
@@ -5,36 +5,45 @@ import java.util.List;
 
 public class SumDigPower {
 
-	public List<Long> sumDigPow(long a, long b) {
+	public List<Long> getMagicNumbers(long startNumber, long endNumber) {
 
-		List<Long> result = new ArrayList<Long>();
+		List<Long> magicNumbers = new ArrayList<Long>();
 
-		for (long i = a; i < b; i++) {
-
-			List<Long> longs = new ArrayList<Long>();
-
-			String temp = Long.toString(i);
-
-			// Split values
-			for (int j = 0; j < temp.length(); j++) {
-				longs.add(Long.valueOf(temp.substring(j, j + 1)));
+		for (long number = startNumber; number < endNumber; number++) {
+			if (isMagicNumber(number)) {
+				magicNumbers.add(number);
 			}
-
-			// Create sum
-			long sum = 0;
-			for (int j = 1; j <= longs.size(); j++) {
-				sum += Math.pow(longs.get(j - 1), j);
-			}
-
-			// Test if sum is equal
-			if (sum == i) {
-				result.add(sum);
-			}
-
 		}
 
-		return result;
+		return magicNumbers;
 
+	}
+	
+	private boolean isMagicNumber(long number) {
+		long value = getMagicNumberValue(extractDigits(number));
+		return value == number;
+	}
+	
+	private long getMagicNumberValue(List<Long> digits) {
+		long sum = 0;
+		for (int position = 1; position <= digits.size(); position++) {
+			sum += Math.pow(digits.get(position - 1), position);
+		}
+		return sum;
+	}
+
+	private List<Long> extractDigits(long number) {
+		List<Long> digits = new ArrayList<Long>();
+		String numberAsString = Long.toString(number);
+
+		for (int j = 0; j < numberAsString.length(); j++) {
+			digits.add(extractDigitAtPosition(numberAsString, j));
+		}
+		return digits;
+	}
+
+	private Long extractDigitAtPosition(String numberAsString, int position) {
+		return Long.valueOf(numberAsString.substring(position, position + 1));
 	}
 
 }
