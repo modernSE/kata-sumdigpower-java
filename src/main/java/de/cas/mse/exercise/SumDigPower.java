52bcd16b722f24610ddea83c7769d8d4621a23ac
modernSE Trainee 2 <keydon+mse-trainee@gmail.com>
2018-02-20 11:22:35 +0100

    Ergebnis der Gruppe 2

--- src/main/java/de/cas/mse/exercise/SumDigPower.java
+++ src/main/java/de/cas/mse/exercise/SumDigPower.java
@@ -1,40 +1,58 @@
 package de.cas.mse.exercise;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SumDigPower {
 
-	public List<Long> sumDigPow(long a, long b) {
+	public List<Long> computeAllSpecialNumbersInRange(long lowerBound, long upperBound) {
 
 		List<Long> result = new ArrayList<Long>();
+		
+		for (long candidate = lowerBound; candidate < upperBound; candidate++) {
+			List<Long> digits = splitDigits(candidate);
 
-		for (long i = a; i < b; i++) {
+			long sum = calculateSumOfPowers(digits);
 
-			List<Long> longs = new ArrayList<Long>();
-
-			String temp = Long.toString(i);
-
-			// Split values
-			for (int j = 0; j < temp.length(); j++) {
-				longs.add(Long.valueOf(temp.substring(j, j + 1)));
-			}
-
-			// Create sum
-			long sum = 0;
-			for (int j = 1; j <= longs.size(); j++) {
-				sum += Math.pow(longs.get(j - 1), j);
-			}
-
-			// Test if sum is equal
-			if (sum == i) {
+			if (sum == candidate) {
 				result.add(sum);
 			}
-
 		}
 
 		return result;
 
 	}
+	
+	private List<Long> splitDigits(long number) {
+		String numberAsString = Long.toString(number);
+		List<Long> digits = new ArrayList<>();
+		
+		for (int position = 0; position < numberAsString.length(); position++) {
+			Long value = digitValueAtPosition(numberAsString, position);
+			digits.add(value);
+		}
+		
+		return digits;
+	}
+
+	private Long digitValueAtPosition(String numberAsString, int position) {
+		String digit = numberAsString.substring(position, position + 1);
+		return Long.valueOf(digit);
+	}
+
+	private long calculateSumOfPowers(List<Long> digits) {
+		long sum = 0;
+		long exponent = 1;
+		
+		for (Long digit : digits) {
+			sum += Math.pow(digit, exponent);
+			exponent += 1;
+		}
+		
+		return sum;
+	}
+
+
 
 }
