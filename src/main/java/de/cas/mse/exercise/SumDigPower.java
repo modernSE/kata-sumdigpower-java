595723d1b983cbbe56b13a7e34ec48f1218d2685
modernSE Trainee 1 <keydon+mse-trainee@gmail.com>
2018-02-20 11:33:46 +0100

    Ergebnis der Gruppe 1

--- src/main/java/de/cas/mse/exercise/SumDigPower.java
+++ src/main/java/de/cas/mse/exercise/SumDigPower.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class SumDigPower {
 
+	// Kommentare müssen weg
 	public List<Long> findDigPowNumbersInRange(long minNumber, long maxNumber) {
 
 		List<Long> digPowNumbers = new ArrayList<Long>();
@@ -22,9 +23,11 @@ public class SumDigPower {
 		return digPowNumbers;
 
 	}
-
+	// Kommentare müssen weg
+	// i steht für 'index' => in der for-Schleife würden wir i stehen lassen (+ nicht konsistent mit dem Rest des Codes)
+	// allDigitsOfCurrentNumber ist ein guter Name
 	private long getPowSumOfNumber(List<Long> allDigitsOfCurrentNumber) {
-		// Create sum
+		// Create sum 
 		long sum = 0;
 		for (int positionOfNumberInContainer = 0; positionOfNumberInContainer < allDigitsOfCurrentNumber.size(); positionOfNumberInContainer++) {
 			Long numberToPow = allDigitsOfCurrentNumber.get(positionOfNumberInContainer);
@@ -33,6 +36,11 @@ public class SumDigPower {
 		return sum;
 	}
 
+	// Kommentare müssen weg
+	// i ist kein guter Parametername
+	// nextCharakterinNumber (Tippfehler + Namenkonventionen + ist kein Char) => besser wäre nextDigitInNumber
+	// Worauf bezieht sich current in currentNumber? Einfach number wäre ausreichend.
+	
 	private List<Long> getAllDigitsOfCurrentNumber(long i) {
 		List<Long> allDigitsOfCurrentNumber = new ArrayList<Long>();
 
