753b1eb288c8cb6c4f01169e23d6bff7398a9ed4
modernSE Trainee 1 <keydon+mse-trainee@gmail.com>
2018-02-20 11:22:31 +0100

    Ergebnis der Gruppe 1

--- src/main/java/de/cas/mse/exercise/SumDigPower.java
+++ src/main/java/de/cas/mse/exercise/SumDigPower.java
@@ -5,36 +5,46 @@ import java.util.List;
 
 public class SumDigPower {
 
-	public List<Long> sumDigPow(long a, long b) {
+	public List<Long> findDigPowNumbersInRange(long minNumber, long maxNumber) {
 
-		List<Long> result = new ArrayList<Long>();
+		List<Long> digPowNumbers = new ArrayList<Long>();
+		for (long currentNumber = minNumber; currentNumber < maxNumber; currentNumber++) {
 
-		for (long i = a; i < b; i++) {
-
-			List<Long> longs = new ArrayList<Long>();
-
-			String temp = Long.toString(i);
-
-			// Split values
-			for (int j = 0; j < temp.length(); j++) {
-				longs.add(Long.valueOf(temp.substring(j, j + 1)));
-			}
-
-			// Create sum
-			long sum = 0;
-			for (int j = 1; j <= longs.size(); j++) {
-				sum += Math.pow(longs.get(j - 1), j);
-			}
+			List<Long> allDigitsOfCurrentNumber = getAllDigitsOfCurrentNumber(currentNumber);
+			long powSum = getPowSumOfNumber(allDigitsOfCurrentNumber);
 
 			// Test if sum is equal
-			if (sum == i) {
-				result.add(sum);
+			if (powSum == currentNumber) {
+				digPowNumbers.add(powSum);
 			}
 
 		}
+		return digPowNumbers;
 
-		return result;
+	}
 
+	private long getPowSumOfNumber(List<Long> allDigitsOfCurrentNumber) {
+		// Create sum
+		long sum = 0;
+		for (int positionOfNumberInContainer = 0; positionOfNumberInContainer < allDigitsOfCurrentNumber.size(); positionOfNumberInContainer++) {
+			Long numberToPow = allDigitsOfCurrentNumber.get(positionOfNumberInContainer);
+			sum += Math.pow(numberToPow, positionOfNumberInContainer + 1);
+		}
+		return sum;
+	}
+
+	private List<Long> getAllDigitsOfCurrentNumber(long i) {
+		List<Long> allDigitsOfCurrentNumber = new ArrayList<Long>();
+
+		String currentNumber = Long.toString(i);
+
+		// Split values
+		for (int j = 0; j < currentNumber.length(); j++) {
+			String nextCharakterinNumber = currentNumber.substring(j, j + 1);
+			Long nextDigit = Long.valueOf(nextCharakterinNumber);
+			allDigitsOfCurrentNumber.add(nextDigit);
+		}
+		return allDigitsOfCurrentNumber;
 	}
 
 }
