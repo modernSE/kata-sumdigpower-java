d8cf3c3e0d4cb695c6b65b20538904b423242245
modernSE Trainee 2 <keydon+mse-trainee@gmail.com>
2018-07-23 11:15:54 +0200

    Ergebnis der Gruppe 2

--- src/main/java/de/cas/mse/exercise/SumDigPower.java
+++ src/main/java/de/cas/mse/exercise/SumDigPower.java
@@ -5,36 +5,48 @@ import java.util.List;
 
 public class SumDigPower {
 
-	public List<Long> sumDigPow(long a, long b) {
-
-		List<Long> result = new ArrayList<Long>();
-
-		for (long i = a; i < b; i++) {
-
-			List<Long> longs = new ArrayList<Long>();
-
-			String temp = Long.toString(i);
-
-			// Split values
-			for (int j = 0; j < temp.length(); j++) {
-				longs.add(Long.valueOf(temp.substring(j, j + 1)));
+	// Fehlende params und return in Javadoc
+	/**
+	 * Returns a sorted list of numbers that fulfill the "Sum-Dig-Power" property
+	 * in the range of [lowerBound, upperBound] (inclusive).
+	 */
+	public List<Long> sumDigPow(long lowerBound, long upperBound) {
+		// Mehrzahl "results" - ?
+		List<Long> result = new ArrayList<>();
+		for (long number = lowerBound; number < upperBound; number++) {
+			// We like to split the long into digits here
+			// splitDigits
+			// calculatePowerSum
+			long powerSum = calculatePowerSumOfDecimalDigits(number);
+			if (powerSum == number) {
+				result.add(powerSum);
 			}
-
-			// Create sum
-			long sum = 0;
-			for (int j = 1; j <= longs.size(); j++) {
-				sum += Math.pow(longs.get(j - 1), j);
-			}
-
-			// Test if sum is equal
-			if (sum == i) {
-				result.add(sum);
-			}
-
 		}
-
 		return result;
+	}
 
+	// Methodenname verwirrt mehr als dass er zum Verständnis beiträgt
+	private long calculatePowerSumOfDecimalDigits(long number) {
+		List<Long> digitsOfNumber = splitIntoDecimalDigits(number);
+		long powerSum = 0;
+		int power = 1;
+		// Use for(int i=...) as you need a Laufvariable for the power :D
+		for (Long digit : digitsOfNumber) {
+			powerSum += Math.pow(digit, power);
+			power++;
+		}
+		return powerSum;
+	}
+
+	private List<Long> splitIntoDecimalDigits(long number) {
+		List<Long> digitsOfNumber = new ArrayList<>();
+		// temp ist kein guter Name. Besser numberAsText
+		String temp = Long.toString(number);
+		for (int i = 0; i < temp.length(); i++) {
+			Long digit = Long.valueOf(temp.substring(i, i + 1));
+			digitsOfNumber.add(digit);
+		}
+		return digitsOfNumber;
 	}
 
 }
