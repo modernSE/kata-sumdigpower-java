b70ce84940dcb3fccdb6a3f1c8b51c67ae446dd6
modernSE Trainee 4 <keydon+mse-trainee@gmail.com>
2018-10-02 13:35:14 +0200

    Ergebnis der Gruppe 4

--- src/main/java/de/cas/mse/exercise/SumDigPowerCalculator.java
+++ src/main/java/de/cas/mse/exercise/SumDigPowerCalculator.java
@@ -1,56 +1,35 @@
 package de.cas.mse.exercise;
 
-import java.util.ArrayList;
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 public class SumDigPowerCalculator {
 
-	// not really calculating anything (or at least, the calculation isn't
-	// important)
 	public List<Long> calculateNumbers(long lowerBound, long upperBound) {
-		List<Long> result = new ArrayList<>();
-		for (long i = lowerBound; i < upperBound; i++) {
-			// ist das nötig?
-			processNumber(i).ifPresent(result::add);
-		}
-		return result;
+		return LongStream.range(lowerBound, upperBound + 1)//
+				.mapToObj(this::processNumber)//
+				.filter(Optional::isPresent)//
+				.map(Optional::get)//
+				.collect(toList());
 	}
 
-	// ist verkompliziert worden. Ursprüngliche Lösung war einfacher zu
-	// verstehen.... und schneller!
 	private Optional<Long> processNumber(long number) {
-		List<Long> digits = extractDigits(number);
-		List<Long> digitPowers = calculatePowers(digits);
-		long sum = calculateSum(digitPowers);
-
-		// schlechter lesbar
-		return sum == number ? Optional.of(number) : Optional.empty();
+		long sum = Long.toString(number).chars()//
+				.mapToObj(c -> String.valueOf((char) c))//
+				.map(Long::valueOf)//
+				.collect(collectingAndThen(toList(), this::calculatePowers))//
+				.sum();
+		return sum == number ? Optional.ofNullable(number) : Optional.empty();
 	}
 
-	private long calculateSum(List<Long> digits) {
-		// ist ok
-		return digits.stream().mapToLong(l -> l).sum();
+	private LongStream calculatePowers(List<Long> digits) {
+		return IntStream.range(0, digits.size())//
+				.mapToDouble(i -> Math.pow(digits.get(i), i + 1))//
+				.mapToLong(d -> (long) d);
 	}
-
-	private List<Long> calculatePowers(List<Long> digits) {
-		List<Long> result = new ArrayList<>();
-		for (int i = 0; i < digits.size(); i++) {
-			result.add((long) Math.pow(digits.get(i), i + 1));
-		}
-		return result;
-	}
-
-	private List<Long> extractDigits(long number) {
-		String numberString = Long.toString(number);
-
-		List<Long> digits = new ArrayList<>();
-		for (int i = 0; i < numberString.length(); i++) {
-			String digitString = String.valueOf(numberString.charAt(i));
-			Long digit = Long.valueOf(digitString);
-			digits.add(digit);
-		}
-		return digits;
-	}
-
 }
