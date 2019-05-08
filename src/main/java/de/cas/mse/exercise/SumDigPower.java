0ec8a1d3184f658c4fc1c439135c434989920c07
modernSE Trainee 2 <keydon+mse-trainee@gmail.com>
2019-05-08 10:37:44 +0200

    Ergebnis der Gruppe 2

--- src/main/java/de/cas/mse/exercise/SumDigPower.java
+++ src/main/java/de/cas/mse/exercise/SumDigPower.java
@@ -5,36 +5,29 @@ import java.util.List;
 
 public class SumDigPower {
 
-	public List<Long> sumDigPow(long a, long b) {
+	// Methodenname klingt so als wenn nur ein Wert berechnet wird.
+	public List<Long> getSumDigPowerOfRange(long startValue, long endValue) { 
 
 		List<Long> result = new ArrayList<Long>();
-
-		for (long i = a; i < b; i++) {
-
-			List<Long> longs = new ArrayList<Long>();
-
-			String temp = Long.toString(i);
-
-			// Split values
-			for (int j = 0; j < temp.length(); j++) {
-				longs.add(Long.valueOf(temp.substring(j, j + 1)));
-			}
-
-			// Create sum
-			long sum = 0;
-			for (int j = 1; j <= longs.size(); j++) {
-				sum += Math.pow(longs.get(j - 1), j);
-			}
-
-			// Test if sum is equal
+		
+		for (long i = startValue; i < endValue; i++) {
+			long sum = calculateSumDigPowOfSingleValue(i);		
 			if (sum == i) {
 				result.add(sum);
 			}
-
 		}
-
 		return result;
-
+	}
+	
+	// gut, dass das extra splitten wegfällt.
+	private long calculateSumDigPowOfSingleValue(long value) {
+		String stringOfValue = Long.toString(value);
+		long sum = 0;
+		for (int i = 0; i < stringOfValue.length(); i++) {
+			int valueOfDigit = Character.getNumericValue(stringOfValue.charAt(i)); 
+			sum += Math.pow(valueOfDigit, i+1);
+		}
+		return sum;
 	}
 
 }
