7a1233c0dc79859de9599d91e4bcd3b523e51017
modernSE Trainee 5 <keydon+mse-trainee@gmail.com>
2019-05-08 10:37:45 +0200

    Ergebnis der Gruppe 5

--- src/main/java/de/cas/mse/exercise/SumDigPower.java
+++ src/main/java/de/cas/mse/exercise/SumDigPower.java
@@ -3,38 +3,47 @@ package de.cas.mse.exercise;
 import java.util.ArrayList;
 import java.util.List;
 
+// Class name anpassen um zu wissen was die Klasse macht
 public class SumDigPower {
 
-	public List<Long> sumDigPow(long a, long b) {
-
-		List<Long> result = new ArrayList<Long>();
-
-		for (long i = a; i < b; i++) {
-
-			List<Long> longs = new ArrayList<Long>();
-
-			String temp = Long.toString(i);
-
-			// Split values
-			for (int j = 0; j < temp.length(); j++) {
-				longs.add(Long.valueOf(temp.substring(j, j + 1)));
+	// vielleicht auch mit kürzerem Namen möglich
+	public List<Long> findNumbersWithSumOfDigitPowersEqualsNumber(long startNumber, long endNumber) {
+		List<Long> results = new ArrayList<Long>();
+		
+		// auslagern in Methode: checkCandidate o.Ä.?
+		for (long currentNumber = startNumber; currentNumber < endNumber; currentNumber++) {
+			List<Long> digitsOfNumber = splitValues(currentNumber);
+			long sum = sumOfDigitPowers(digitsOfNumber);
+	
+			if (sum == currentNumber) {
+				results.add(sum);
 			}
-
-			// Create sum
-			long sum = 0;
-			for (int j = 1; j <= longs.size(); j++) {
-				sum += Math.pow(longs.get(j - 1), j);
-			}
-
-			// Test if sum is equal
-			if (sum == i) {
-				result.add(sum);
-			}
-
 		}
-
-		return result;
-
+		return results;
+	}
+	
+	//Einzahl?
+	List<Long> splitValues(long numberToSplit) {
+		List<Long> digitsOfNumber = new ArrayList<Long>();
+		String  stringOfNumber = Long.toString(numberToSplit);
+		
+		// i statt j?
+		for (int j = 0; j < stringOfNumber.length(); j++) {
+			digitsOfNumber.add(Long.valueOf(stringOfNumber.substring(j, j + 1)));
+		}
+		return digitsOfNumber;
+	}
+	
+	
+	long sumOfDigitPowers (List<Long> digitsOfNumber) {
+		// sum statt result?
+		long result = 0; 
+		
+		// i statt j
+		for (int j = 0; j <= digitsOfNumber.size() -1; j++) {
+			result += Math.pow(digitsOfNumber.get(j), j+1);
+		}
+		return result; 
 	}
 
 }
