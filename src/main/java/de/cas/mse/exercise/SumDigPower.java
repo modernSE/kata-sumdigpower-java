aad5e415390a844dfc90a36196c87d9cbed8d402
modernSE Trainee 3 <keydon+mse-trainee@gmail.com>
2019-05-08 10:37:42 +0200

    Ergebnis der Gruppe 3

--- src/main/java/de/cas/mse/exercise/SumDigPower.java
+++ src/main/java/de/cas/mse/exercise/SumDigPower.java
@@ -2,39 +2,43 @@ package de.cas.mse.exercise;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class SumDigPower {
-
-	public List<Long> sumDigPow(long a, long b) {
-
+public class SumDigPower { //class umbenennen? 
+	//methodenname zu(?) lang 
+	
+	//müssen es longs sein? sind doch immer integer - alles andere wäre fehlerhaft? 
+	public List<Long> findNumbersWhereSumOfDigitPowersIsEqualToTheNumberItselfInInterval(long lowerBound, long upperBound) {
 		List<Long> result = new ArrayList<Long>();
 
-		for (long i = a; i < b; i++) {
+		for (long number = lowerBound; number < upperBound; number++) { //laufvariable umbenannt?
+			long sum = calculateSumOfPowers(extractDigitsFromNumber(number));
 
-			List<Long> longs = new ArrayList<Long>();
-
-			String temp = Long.toString(i);
-
-			// Split values
-			for (int j = 0; j < temp.length(); j++) {
-				longs.add(Long.valueOf(temp.substring(j, j + 1)));
-			}
-
-			// Create sum
-			long sum = 0;
-			for (int j = 1; j <= longs.size(); j++) {
-				sum += Math.pow(longs.get(j - 1), j);
-			}
-
-			// Test if sum is equal
-			if (sum == i) {
+			if (sum == number) {
 				result.add(sum);
 			}
-
 		}
 
 		return result;
+	}
+	private long calculateSumOfPowers(List<Long> digits) {
+		long sum = 0;
+		for (int i = 1; i <= digits.size(); i++) {
+			sum += Math.pow(digits.get(i - 1), i);
+		}
+		return sum;
+	}
 
+	private List<Long> extractDigitsFromNumber(long number) {
+		List<Long> digits = new ArrayList<Long>();
+
+		String numberAsString = Long.toString(number);
+		
+		for (int j = 0; j < numberAsString.length(); j++) {
+			digits.add(Long.valueOf(numberAsString.substring(j, j + 1)));
+		}
+		
+		return digits;
 	}
 
 }
