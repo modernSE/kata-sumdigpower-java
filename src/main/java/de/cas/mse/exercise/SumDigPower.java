e47c9aeae2faf10b67e4eb928324fc8036fb971d
modernSE Trainee 1 <keydon+mse-trainee@gmail.com>
2019-05-08 10:37:45 +0200

    Ergebnis der Gruppe 1

--- src/main/java/de/cas/mse/exercise/SumDigPower.java
+++ src/main/java/de/cas/mse/exercise/SumDigPower.java
@@ -5,36 +5,48 @@ import java.util.List;
 
 public class SumDigPower {
 
-	public List<Long> sumDigPow(long a, long b) {
+	/**
+	 * 
+	 * @param startNumber
+	 * @param endNumber
+	 * @return all values where a number is equal to the sum of its digits, raised to the power of their position in the number.
+	 */
+	public List<Long> findFittingNumbers(long startNumber, long endNumber) {
 
 		List<Long> result = new ArrayList<Long>();
 
-		for (long i = a; i < b; i++) {
+		for (long currentNumber = startNumber; currentNumber < endNumber; currentNumber++) {
+			
+			List<Long> digits = splitNumberIntoDigits(currentNumber);
 
-			List<Long> longs = new ArrayList<Long>();
+			long powSum = calculatePowSumOfDigits(digits);
 
-			String temp = Long.toString(i);
-
-			// Split values
-			for (int j = 0; j < temp.length(); j++) {
-				longs.add(Long.valueOf(temp.substring(j, j + 1)));
-			}
-
-			// Create sum
-			long sum = 0;
-			for (int j = 1; j <= longs.size(); j++) {
-				sum += Math.pow(longs.get(j - 1), j);
-			}
-
-			// Test if sum is equal
-			if (sum == i) {
-				result.add(sum);
+			if (powSum == currentNumber) {
+				result.add(powSum);
 			}
 
 		}
-
 		return result;
-
+	}
+	
+	private List<Long> splitNumberIntoDigits(long number) {
+		List<Long> digits = new ArrayList<>();
+		
+		String numberString = Long.toString(number);
+		for (int i = 0; i < numberString.length(); i++) {
+			char digit = numberString.charAt(i);
+			digits.add(Long.valueOf(Character.getNumericValue(digit)));
+		}		
+		return digits;		
+	}
+	
+	private long calculatePowSumOfDigits(List<Long> digits) {
+		long sum = 0;
+		for (int i = 0; i < digits.size(); i++) {
+			sum += Math.pow(digits.get(i), i + 1);
+		}
+		
+		return sum;
 	}
 
 }
