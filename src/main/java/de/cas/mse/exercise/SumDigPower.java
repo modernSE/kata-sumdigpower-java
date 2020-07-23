6630f8fdded9cbb315be859afdaec8a2af669560
hpap <h.papist@live.de>
2020-07-23 08:06:06 +0000

    Group2 refactoring

--- src/main/java/de/cas/mse/exercise/SumDigPower.java
+++ src/main/java/de/cas/mse/exercise/SumDigPower.java
@@ -5,37 +5,39 @@ import java.util.List;
 
 public class SumDigPower {
 
-	public List<Long> sumDigPow(long a, long b) {
+	public List<Long> sumDigPow(long rangeStart, long rangeEnd) {
 
 		List<Long> result = new ArrayList<Long>();
 
-		for (long i = a; i < b; i++) {
-
-			List<Long> longs = new ArrayList<Long>();
-
-			String temp = Long.toString(i);
-
-			// Split values
-			for (int j = 0; j < temp.length(); j++) {
-				longs.add(Long.valueOf(temp.substring(j, j + 1)));
+		for (long currentNumber = rangeStart; currentNumber < rangeEnd; currentNumber++) {
+            if (checkNumber(currentNumber)) {
+				result.add(currentNumber);
 			}
-
-			// Create sum
-			long sum = 0;
-			for (int j = 1; j <= longs.size(); j++) {
-				sum += Math.pow(longs.get(j - 1), j);
-			}
-
-			// Test if sum is equal
-			if (sum == i) {
-				result.add(sum);
-			}
-
 		}
-
         System.out.println(result);
 		return result;
+    }
+    
+    private boolean checkNumber(Long numberToCheck) {
+        List<Long> digits = splitNumberIntoDigits(Long.toString(numberToCheck));        
+        
+        return calculateSumDigPower(digits) == numberToCheck;
+    }
 
-	}
+    private List<Long> splitNumberIntoDigits(String currentNumber) {
+        List<Long> digits = new ArrayList<>();
+        for (int digitIndex = 0; digitIndex < currentNumber.length(); digitIndex++) {
+            digits.add(Long.valueOf(currentNumber.substring(digitIndex, digitIndex + 1)));
+        }
+        return digits;
+    }
+
+    private long calculateSumDigPower(List<Long> digits) {
+        long sum = 0;
+        for (int digitIndex = 1; digitIndex <= digits.size(); digitIndex++) {
+            sum += Math.pow(digits.get(digitIndex - 1), digitIndex);
+        }
+        return sum;
+    }
 
 }
