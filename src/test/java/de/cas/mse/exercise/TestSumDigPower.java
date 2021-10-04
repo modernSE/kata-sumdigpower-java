d86c96220aff706aef1ee5c9a70f3f1d1217a02c
Mabian <taskan.barnak@gmail.com>
2021-10-04 13:29:36 +0000

    Gruppe 1

--- src/test/java/de/cas/mse/exercise/TestSumDigPower.java
+++ src/test/java/de/cas/mse/exercise/TestSumDigPower.java
@@ -16,26 +16,26 @@ public class TestSumDigPower {
 
 	@Test
 	public void tenTo150_contains89and135() {
-		assertThat(sumDigPower.sumDigPow(10, 150)).containsExactly(89l, 135l);
+		assertThat(sumDigPower.getEurekaNumbersInRange(10, 150)).containsExactly(89l, 135l);
 	}
 
 	@Test
 	public void ninetyTo100_isEmpty() {
-		assertThat(sumDigPower.sumDigPow(90, 100)).isEmpty();
+		assertThat(sumDigPower.getEurekaNumbersInRange(90, 100)).isEmpty();
 	}
 
 	@Test
 	public void zeroTo100_firstTenNumbersAnd89ShoudBeContained() {
-		assertThat(sumDigPower.sumDigPow(1, 100)).containsExactly(1l, 2l, 3l, 4l, 5l, 6l, 7l, 8l, 9l, 89l);
+		assertThat(sumDigPower.getEurekaNumbersInRange(1, 100)).containsExactly(1l, 2l, 3l, 4l, 5l, 6l, 7l, 8l, 9l, 89l);
 	}
 
 	@Test
 	public void tenTo100_only89Contained() {
-		assertThat(sumDigPower.sumDigPow(10, 100)).containsExactly(89l);
+		assertThat(sumDigPower.getEurekaNumbersInRange(10, 100)).containsExactly(89l);
 	}
 
 	@Test
 	public void firstTenNumbers_shouldBeAllContained() {
-		assertThat(sumDigPower.sumDigPow(1, 10)).containsExactly(1l, 2l, 3l, 4l, 5l, 6l, 7l, 8l, 9l);
+		assertThat(sumDigPower.getEurekaNumbersInRange(1, 10)).containsExactly(1l, 2l, 3l, 4l, 5l, 6l, 7l, 8l, 9l);
 	}
 }
