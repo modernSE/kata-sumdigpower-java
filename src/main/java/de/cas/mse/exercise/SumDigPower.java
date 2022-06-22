d56e20b4b7ab4b89cab372ea98c0b5ed2f8713ed
SaltyCorvus <luxwxe@gmail.com>
2022-06-22 14:34:56 +0000

    Gruppe 1

--- src/main/java/de/cas/mse/exercise/SumDigPower.java
+++ src/main/java/de/cas/mse/exercise/SumDigPower.java
@@ -5,26 +5,19 @@ import java.util.List;
 
 public class SumDigPower {
 
-	public List<Long> sumDigPow(long a, long b) {
+	public List<Long> sumOfDigitsPowered(long startOfInterval, long endOfInterval) {
 
 		List<Long> result = new ArrayList<Long>();
 
-		for (long i = a; i < b; i++) {
+		for (long i = startOfInterval; i < endOfInterval; i++) {
 
-			List<Long> longs = new ArrayList<Long>();
+			List<Long> digits = new ArrayList<Long>();
 
-			String temp = Long.toString(i);
+			String currentNumber = Long.toString(i);
 
-			// Split values
-			for (int j = 0; j < temp.length(); j++) {
-				longs.add(Long.valueOf(temp.substring(j, j + 1)));
-			}
+			List<Long> splittedCurrentNumber = splitCurrentNumber(digits, currentNumber);
 
-			// Create sum
-			long sum = 0;
-			for (int j = 1; j <= longs.size(); j++) {
-				sum += Math.pow(longs.get(j - 1), j);
-			}
+			long sum = createSum(splittedCurrentNumber);
 
 			// Test if sum is equal
 			if (sum == i) {
@@ -35,7 +28,21 @@ public class SumDigPower {
 
         System.out.println(result);
 		return result;
+	}
 
+	private List<Long> splitCurrentNumber(List<Long> digits,String currentNumber) {
+		for (int j = 0; j < currentNumber.length(); j++) {
+			digits.add(Long.valueOf(currentNumber.substring(j, j + 1)));
+		}
+		return digits;
+	}
+
+	private long createSum(List<Long> digits) {
+		long sum = 0;
+		for (int j = 1; j <= digits.size(); j++) {
+			sum += Math.pow(digits.get(j - 1), j);
+		}
+		return sum;
 	}
 
 }
