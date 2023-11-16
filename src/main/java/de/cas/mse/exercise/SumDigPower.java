972d42afdfa41849465f7d5359c2cc0f5d95a187
Fabian Sorn <fabian.sorn@icloud.com>
2023-11-16 15:10:06 +0000

    10/10 best refactoring ever, would recommend again

--- src/main/java/de/cas/mse/exercise/SumDigPower.java
+++ src/main/java/de/cas/mse/exercise/SumDigPower.java
@@ -5,37 +5,49 @@ import java.util.List;
 
 public class SumDigPower {
 
-	public List<Long> sumDigPow(long a, long b) {
-
+	public List<Long> getEurekaNumbers(long start, long exclusiveEnd) {
 		List<Long> result = new ArrayList<Long>();
 
-		for (long i = a; i < b; i++) {
+		for (long candidate = start; candidate < exclusiveEnd; candidate++) {
+			List<Long> digits = getDigits(candidate);
+			long eurekaSum = getEurekaSum(digits);
 
-			List<Long> longs = new ArrayList<Long>();
-
-			String temp = Long.toString(i);
-
-			// Split values
-			for (int j = 0; j < temp.length(); j++) {
-				longs.add(Long.valueOf(temp.substring(j, j + 1)));
+			if (eurekaSum == candidate) {
+				 result.add(eurekaSum);
 			}
-
-			// Create sum
-			long sum = 0;
-			for (int j = 1; j <= longs.size(); j++) {
-				sum += Math.pow(longs.get(j - 1), j);
-			}
-
-			// Test if sum is equal
-			if (sum == i) {
-				result.add(sum);
-			}
-
 		}
 
         System.out.println(result);
 		return result;
 
+	}
+
+	private long getEurekaSum(List<Long> digits) {
+		long sum = 0;
+
+		for (int exponent = 1; exponent <= digits.size(); exponent++) {
+			int index = exponent - 1;
+			sum += Math.pow(digits.get(index), exponent);
+		}
+
+		return sum;
+	}
+
+	private List<Long> getDigits(long candidate) {
+		String candidateString = Long.toString(candidate);
+		List<Long> digits = new ArrayList<Long>();
+
+		for (int index = 0; index < candidateString.length(); index++) {
+			long digit = getDigitAtIndex(candidateString, index);
+			digits.add(digit); 
+		}
+
+		return digits;
+	}
+
+	private Long getDigitAtIndex(String candidateString, int index) {
+		String digit = candidateString.substring(index, index + 1);
+		return Long.valueOf(digit);
 	}
 
 }
