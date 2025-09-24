5f76ab07721991bfb69b94eef65b10111d4b678f
Lukas Bugaj <lukas.bugaj@cas.de>
2025-09-24 17:02:23 +0200

    Solution based on streams and helper records

--- src/main/java/de/cas/mse/exercise/SumDigPower.java
+++ src/main/java/de/cas/mse/exercise/SumDigPower.java
@@ -1,41 +1,45 @@
 package de.cas.mse.exercise;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 public class SumDigPower {
 
-	public List<Long> sumDigPow(long a, long b) {
-
-		List<Long> result = new ArrayList<Long>();
-
-		for (long i = a; i < b; i++) {
-
-			List<Long> longs = new ArrayList<Long>();
-
-			String temp = Long.toString(i);
-
-			// Split values
-			for (int j = 0; j < temp.length(); j++) {
-				longs.add(Long.valueOf(temp.substring(j, j + 1)));
-			}
-
-			// Create sum
-			long sum = 0;
-			for (int j = 1; j <= longs.size(); j++) {
-				sum += Math.pow(longs.get(j - 1), j);
-			}
-
-			// Test if sum is equal
-			if (sum == i) {
-				result.add(sum);
-			}
-
-		}
-
-        System.out.println(result);
-		return result;
-
+	public List<Long> sumDigPow(long startInclusive, long endExclusive) {
+		return LongStream
+			.range(startInclusive, endExclusive)
+			.filter(this::isEureka)
+			.boxed()
+			.toList();
 	}
 
+	boolean isEureka(Long candidate) {
+		var sumOfConsecutivePowers = Number.of(candidate)
+			.streamDigits()
+			.mapToDouble(digit -> Math.pow(digit.asInt(), digit.position()))
+			.sum();
+
+		return candidate == sumOfConsecutivePowers;
+	}
+
+	record Number(String number){
+		static Number of(Long num){
+			return new Number(num.toString());
+		}
+
+		Digit digitAt(Integer index){
+			var digitAsChar = number.charAt(index);
+			var digitAsString = Character.toString(digitAsChar);
+			return new Digit(Integer.valueOf(digitAsString), index + 1);
+		}
+
+		Stream<Digit> streamDigits() {
+			return IntStream.range(0, number.length())
+				.mapToObj(this::digitAt);
+		}
+	}
+
+	record Digit(Integer asInt, Integer position){}
 }
