d04f61243dbee98ad3f9f0bb42240fa81fa5c16e
thingucas <thinh.nguyen@cas.de>
2025-09-25 14:27:21 +0000

    Gruppe 5

--- src/main/java/de/cas/mse/exercise/SumDigPower.java
+++ src/main/java/de/cas/mse/exercise/SumDigPower.java
@@ -5,37 +5,40 @@ import java.util.List;
 
 public class SumDigPower {
 
-	public List<Long> sumDigPow(long a, long b) {
+	public List<Long> getNumbersInRange(long lowerLimit, long upperLimit) {
 
 		List<Long> result = new ArrayList<Long>();
 
-		for (long i = a; i < b; i++) {
+		for (long currentNumber = lowerLimit; currentNumber < upperLimit; currentNumber++) {
 
-			List<Long> longs = new ArrayList<Long>();
+			List<Long> digits = getDigits(currentNumber);
+			long sum = computeSum(digits);
 
-			String temp = Long.toString(i);
-
-			// Split values
-			for (int j = 0; j < temp.length(); j++) {
-				longs.add(Long.valueOf(temp.substring(j, j + 1)));
-			}
-
-			// Create sum
-			long sum = 0;
-			for (int j = 1; j <= longs.size(); j++) {
-				sum += Math.pow(longs.get(j - 1), j);
-			}
-
-			// Test if sum is equal
-			if (sum == i) {
+			if (sum == currentNumber) {
 				result.add(sum);
 			}
-
 		}
 
-        System.out.println(result);
+		System.out.println(result);
 		return result;
 
+	}
+
+	private long computeSum(List<Long> digits) {
+		long sum = 0;
+		for (int j = 1; j <= digits.size(); j++) {
+			sum += Math.pow(digits.get(j - 1), j);
+		}
+		return sum;
+	}
+
+	private List<Long> getDigits(long currentNumber) {
+		char[] digits = Long.toString(currentNumber).toCharArray();
+
+		return List.of(digits).stream() //
+				.map(String::new)
+				.map(Long::parseLong) //
+				.toList();
 	}
 
 }
